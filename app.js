7e4a25b118bf07f789203fea50fa384b2baf44e6
James Young(Goodpudding) <james.trey.young@gmail.com>
2023-01-09 13:58:50 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
+const fs = require('fs'); // include the filesystem module
 
-function pickACat(){
-  var specificLNum = 0;
-function randomLNum(min, max) {
-    // This is to get a random number within 1 - 15 so it can pick a photo from the folder 
-    var specificLNum = Math.floor(Math.random() * (max - min) + min);
-    console.log(specificLNum);
-    return specificLNum;
-    
-  }
-  function randomRNum(min, max) {
-    var specificRNum = Math.floor(Math.random() * (max - min) + min);
-    if (specificRNum === specificLNum){
-      console.log("reroll")
-      randomRNum();
-    }
-    else{
-    // This is to get a random number within 1 - 15 so it can pick a photo from the folder 
-    
-    console.log(specificRNum);
-    return specificRNum;
-    }
+// get a list of all the photos in the folder
+fs.readdir('/path/to/photos/folder', (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
   }
 
-function pickLeftPic(){
-    // This is to pick in and display the random photo on the left
-    var leftNum = randomLNum(1,15);
-    console.log(leftNum);
-    document.write("<img src='images/CatPhotos/" + leftNum + ".jpg' style='max-width: 400px;' style='max-height: 400px;' />")
-}
-randomRNum();
-function pickRightPic(){
-    // This is to pick in and display the random photo on the left
-    var rightNum = randomRNum(1,15);
-    console.log(rightNum);
-    document.write("<img src='images/CatPhotos/" + rightNum + ".jpg' style='max-width: 400px;' style='max-height: 400px;' />")
-    }
-    pickLeftPic();
-    pickRightPic();
+  // select two random photos from the list
+  const photo1Index = Math.floor(Math.random() * files.length);
+  let photo2Index = Math.floor(Math.random() * (files.length - 1));
+  if (photo2Index >= photo1Index) {
+    photo2Index += 1;
   }
+  const photo1 = files[photo1Index];
+  const photo2 = files[photo2Index];
+
+  // display the photos on the page
+  document.getElementById('photo1').src = '/path/to/photos/folder/' + photo1;
+  document.getElementById('photo2').src = '/path/to/photos/folder/' + photo2;
+});
+
+// handle the user's vote
+document.getElementById('vote1').addEventListener('click', () => {
+  // update the vote count for photo1 in the database
+});
+document.getElementById('vote2').addEventListener('click', () => {
+  // update the vote count for photo2 in the database
+});
+const mysql = require('mysql'); // include the MySQL module
+
+const connection = mysql.createConnection({
+  host: 'localhost', // the hostname of the database server
+  user: 'user', // the username for the database
+  password: 'password', // the password for the database
+  database: 'database' // the name of the database
+});
+
+// update the vote count for photo1
+connection.query('UPDATE photos SET votes = votes + 1 WHERE id = ?', [photo1Id], (error, results) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`Updated vote count for photo ${photo1Id}`);
+  }
+});
+
+// update the vote count for photo2
+connection.query('UPDATE photos SET votes = votes + 1 WHERE id = ?', [photo2Id], (error, results) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`Updated vote count for photo ${photo2Id}`);
+  }
+});
+
+connection.end(); // close the connection to the database
